c9c06532206466dfe03fadf8a6ccfefd8ccdc06e
bz-lin <56818112+bz-lin@users.noreply.github.com>
2020-04-26 20:56:57 +0800

    Update bulid_rover.sh

--- bulid_rover.sh
+++ bulid_rover.sh
@@ -1,12 +1,25 @@
-### build monitor with raspberry buster lite img ###
+### build rover with raspberry buster lite img ###
 
 ##enable ssh service##
 systemctl enable ssh
 systemctl restart ssh
 
-
 ##chage hostname##
+echo "rover" > /etc/hostname
+sed -i 6{s/raspberrypi/rover/g} /etc/hosts
 #hostnamectl set-hostname monitor
+
+##set wifi##
+cat <<EOT >> /etc/wpa_supplicant/wpa_supplicant.conf
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=TW
+
+network={
+        ssid="monitor"
+        psk="49937000"
+}
+EOT
 
 ##change password for pi##
 echo pi:rover | chpasswd
@@ -27,7 +40,12 @@ apt update
 apt install vsftpd -y
 echo "write_enable=Yes" >> /etc/vsftpd.conf
 
-##install kivy ver.1.11.1##
-apt install -y python3-pip
+##install pip3 & pigpio##
+apt install -y python3-pip python3-pigpio
 
+##install pyserial##
 pip3 install pyserial
+
+##enable pigpio service##
+systemctl enable pigpiod
+
